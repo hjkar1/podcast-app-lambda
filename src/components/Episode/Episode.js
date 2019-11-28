3bbab7c22a751206dde976d8b60aa583b72308fb
hjkar1 <hjkar1@outlook.com>
2019-11-28 23:52:28 +0200

    change episode description text styling

--- src/components/Episode/Episode.js
+++ src/components/Episode/Episode.js
@@ -24,6 +24,7 @@ const styles = theme => ({
   description: {
     lineHeight: '1.5',
     margin: theme.spacing(2),
+    overflowWrap: 'break-word',
     [theme.breakpoints.up('sm')]: {
       width: '50%',
       margin: 'auto'
